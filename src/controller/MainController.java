77780b0094b172b7f0e139bd6a2be571417019ec
francisdiaz93 <fjdiazjimenez93@gmail.com>
2025-04-15 17:23:10 +0200

    Implementación de clases de la base de datos para eliminar, introducir y ver datos.

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import java.io.IOException;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainController {
 
@@ -15,6 +17,10 @@ public class MainController {
     private void mostrarProductos() {
         cargarVista("/view/ProductosView.fxml");
     }
+    
+   /* private void añadirProductos() {
+    	cargarVista("/view/ProductosView.fxml");
+    }*/
 
     @FXML
     private void mostrarEntradas() {
@@ -38,11 +44,19 @@ public class MainController {
 
     private void cargarVista(String ruta) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(ruta));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ProductsView.fxml"));
             Pane nuevaVista = loader.load();
             panelPrincipal.getChildren().setAll(nuevaVista);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    /*public void initialize() {
+        // Cargar el logo al iniciar
+        Image logo = new Image(getClass().getResource("/images/LOGO.png").toExternalForm());
+        ImageView logoView = new ImageView(logo);
+        logoView.setFitWidth(300); // Ajusta el tamaño si quieres
+        logoView.setPreserveRatio(true);
+    }*/
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module Gestion_almacen {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens controller to javafx.fxml;
